2bc0fdac4531e8eebf2fe677c6033278fba211b1
Wahaj Farooq <wahaj@Wahajs-MacBook-Air.local>
2026-08-23 16:02:56 -0700

    Fix Sanity Studio black screen by adding force-dynamic route export and simplifying StudioClient

--- app/studio/[[...tool]]/StudioClient.jsx
+++ app/studio/[[...tool]]/StudioClient.jsx
@@ -4,20 +4,5 @@ import { NextStudio } from 'next-sanity/studio';
 import config from '../../../sanity.config';
 
 export default function StudioClient() {
-  return (
-    <div style={{
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      width: '100vw',
-      height: '100vh',
-      zIndex: 999999,
-      backgroundColor: '#0e0e10',
-      margin: 0,
-      padding: 0,
-      overflow: 'auto'
-    }}>
-      <NextStudio config={config} />
-    </div>
-  );
+  return <NextStudio config={config} />;
 }
--- app/studio/[[...tool]]/page.jsx
+++ app/studio/[[...tool]]/page.jsx
@@ -1,5 +1,7 @@
 import StudioClient from './StudioClient';
 
+export const dynamic = 'force-dynamic';
+
 export function generateStaticParams() {
   return [{ tool: [] }];
 }
