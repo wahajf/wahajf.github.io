b4b0adc5186c49532c7a5bda83489630a242f3fb
Wahaj Farooq <wahaj@Wahajs-MacBook-Air.local>
2026-08-23 16:05:20 -0700

    Fix Sanity Studio layout by stretching Studio flex container to 100vw x 100vh and removing white margins

--- app/studio/[[...tool]]/StudioClient.jsx
+++ app/studio/[[...tool]]/StudioClient.jsx
@@ -4,5 +4,21 @@ import { NextStudio } from 'next-sanity/studio';
 import config from '../../../sanity.config';
 
 export default function StudioClient() {
-  return <NextStudio config={config} />;
+  return (
+    <div style={{
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      width: '100vw',
+      height: '100vh',
+      zIndex: 999999,
+      backgroundColor: '#0e0e10',
+      display: 'flex',
+      flexDirection: 'column'
+    }}>
+      <NextStudio config={config} />
+    </div>
+  );
 }
